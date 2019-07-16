d5ef30a35dce99b6dd8ff62e21ddcab9ea2168a3
Dominika Piechota <piechota.domi@gmail.com>
2019-07-16 22:44:50 +0200

    navigating to hours page after logging in

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,7 +11,8 @@ export class LoginComponent implements OnInit {
   username = 'domi';
   password = '';
 
-  constructor() {
+  constructor(
+    private router: Router) {
   }
 
   ngOnInit() {
@@ -18,5 +20,6 @@ export class LoginComponent implements OnInit {
 
   handleLogin() {
     console.log('login');
+    this.router.navigate(['hours']);
   }
 }
