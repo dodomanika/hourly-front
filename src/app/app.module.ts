a1cebf301d19ddc30f933703498e8c611024de84
Dominika Piechota <piechota.domi@gmail.com>
2019-07-16 22:42:16 +0200

    adding hours-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import {FormsModule} from '@angular/forms';
+import { HoursListComponent } from './hours-list/hours-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HoursListComponent
   ],
   imports: [
     BrowserModule,
