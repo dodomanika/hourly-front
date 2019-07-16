d048c9e494e9ed37d78a2dc1843bd16a6c414d07
Dominika Piechota <piechota.domi@gmail.com>
2019-07-16 23:08:08 +0200

    adding the route for logout page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {LoginComponent} from './login/login.component';
 import {HoursListComponent} from './hours-list/hours-list.component';
+import {LogoutComponent} from './logout/logout.component';
 
 const routes: Routes = [
   {path: '', component: LoginComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'hours', component: HoursListComponent}
+  {path: 'hours', component: HoursListComponent},
+  {path: 'logout', component: LogoutComponent}
 ];
 
 @NgModule({
