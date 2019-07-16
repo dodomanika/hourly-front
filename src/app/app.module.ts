84718466676520536179dcba8a756c5a81e3652b
Dominika Piechota <piechota.domi@gmail.com>
2019-07-16 23:07:10 +0200

    adding the logout component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import {FormsModule} from '@angular/forms';
 import { HoursListComponent } from './hours-list/hours-list.component';
 import { MenuComponent } from './menu/menu.component';
 import { FooterComponent } from './footer/footer.component';
+import { LogoutComponent } from './logout/logout.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { FooterComponent } from './footer/footer.component';
     LoginComponent,
     HoursListComponent,
     MenuComponent,
-    FooterComponent
+    FooterComponent,
+    LogoutComponent
   ],
   imports: [
     BrowserModule,
