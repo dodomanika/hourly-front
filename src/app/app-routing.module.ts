8065b1da46afe310651cc914ca0630510507b808
Dominika Piechota <piechota.domi@gmail.com>
2019-07-16 22:43:03 +0200

    adding route for hours-list component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {LoginComponent} from './login/login.component';
+import {HoursListComponent} from './hours-list/hours-list.component';
 
 const routes: Routes = [
   {path: '', component: LoginComponent},
-  {path: 'login', component: LoginComponent}
+  {path: 'login', component: LoginComponent},
+  {path: 'hours', component: HoursListComponent}
 ];
 
 @NgModule({
