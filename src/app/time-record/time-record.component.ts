7946308a217328a575539aefe1f48803ee8eeac0
Dominika Piechota <piechota.domi@gmail.com>
2019-07-17 21:55:48 +0200

    adding time record component for adding and updating the time record

---
+++ src/app/time-record/time-record.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-time-record',
+  templateUrl: './time-record.component.html',
+  styleUrls: ['./time-record.component.css']
+})
+export class TimeRecordComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
