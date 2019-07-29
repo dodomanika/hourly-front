e780af1596c175ec1ff843b03ffff1bb8bcc632d
Dominika Piechota <piechota.domi@gmail.com>
2019-07-29 21:12:11 +0200

    implementing the canActivate method in the route guard service

--- src/app/service/route-guard.service.ts
+++ src/app/service/route-guard.service.ts
@@ -1,9 +1,24 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from '@angular/router';
+import {BasicAuthenticationService} from './basic-authentication.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class RouteGuardService {
+export class RouteGuardService implements CanActivate {
 
-  constructor() { }
+  constructor(
+    private basicAuthenticationService: BasicAuthenticationService,
+    private router: Router
+  ) {
+  }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    if (this.basicAuthenticationService.isUserLoggedIn()) {
+      return true;
+    }
+
+    this.router.navigate(['login']);
+    return false;
+  }
 }
