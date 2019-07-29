df1d22a5341bda3fce2808dd81d7a6e7245728b4
Dominika Piechota <piechota.domi@gmail.com>
2019-07-29 21:13:45 +0200

    checking the route guard service for each view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,14 @@ import {LoginComponent} from './login/login.component';
 import {HoursListComponent} from './hours-list/hours-list.component';
 import {LogoutComponent} from './logout/logout.component';
 import {TimeRecordComponent} from './time-record/time-record.component';
+import {RouteGuardService} from './service/route-guard.service';
 
 const routes: Routes = [
   {path: '', component: LoginComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'hours/:dateIndex', component: HoursListComponent},
-  {path: 'logout', component: LogoutComponent},
-  {path: 'hours/:dateIndex/:id', component: TimeRecordComponent}
+  {path: 'hours/:dateIndex', component: HoursListComponent, canActivate: [RouteGuardService]},
+  {path: 'logout', component: LogoutComponent, canActivate: [RouteGuardService]},
+  {path: 'hours/:dateIndex/:id', component: TimeRecordComponent, canActivate: [RouteGuardService]}
 ];
 
 @NgModule({
