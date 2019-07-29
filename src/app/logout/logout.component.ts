cb3fe79a554a71e45666e38508e7b482ae368fc2
Dominika Piechota <piechota.domi@gmail.com>
2019-07-29 21:25:54 +0200

    logging out added

--- src/app/logout/logout.component.ts
+++ src/app/logout/logout.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {BasicAuthenticationService} from '../service/basic-authentication.service';
 
 @Component({
   selector: 'app-logout',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LogoutComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private basicAuthorisationService: BasicAuthenticationService
+  ) {
+  }
 
   ngOnInit() {
+    this.basicAuthorisationService.logout();
   }
 
 }
