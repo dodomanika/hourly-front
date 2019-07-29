221822d96a65e947f5efc940a0e850b089ab54f0
Dominika Piechota <piechota.domi@gmail.com>
2019-07-29 21:22:51 +0200

    refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import {FormsModule} from '@angular/forms';
 import { HoursListComponent } from './hours-list/hours-list.component';
 import { MenuComponent } from './menu/menu.component';
 import { FooterComponent } from './footer/footer.component';
