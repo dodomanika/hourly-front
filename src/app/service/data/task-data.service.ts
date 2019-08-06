a7e07fd01df5e280ed7567a62c16f208e8f4a030
Dominika Piechota <piechota.domi@gmail.com>
2019-08-06 21:49:29 +0200

    adding methods for task requests

--- src/app/service/data/task-data.service.ts
+++ src/app/service/data/task-data.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import {API_URL} from '../../app.constants';
+import {HttpClient} from '@angular/common/http';
+import {Task} from '../../task-manage/task-manage.component';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskDataService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  retrieveTasks() {
+    return this.http.get<Task[]>(`${API_URL}/tasks`);
+  }
+
+  deleteTask(id) {
+    return this.http.delete(`${API_URL}/tasks/${id}`);
+  }
+
+  retrieveTask(id) {
+    return this.http.get<Task>(`${API_URL}/tasks/${id}`);
+  }
+
+  postTask(task) {
+    return this.http.post(`${API_URL}/tasks`, task);
+  }
+
+  putTask(id, task) {
+    return this.http.put(`${API_URL}/tasks/${id}`, task);
+
+  }
 }
